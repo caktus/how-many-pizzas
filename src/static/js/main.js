f435c49b18961ae8bb93364809b9d1666333c144
Mark Lavin <mlavin@caktusgroup.com>
2015-07-10 08:58:47 -0400

    Minimal working calculator for pizzas --> people.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -2,4 +2,18 @@
 (function ($) {
     'use strict';
 
+    function peopleToPizzas(people) {
+        return Math.ceil(people * 3 / 8);
+    }
+
+    function pluralize(count) {
+        return count === 1 ?  '' : 's';
+    }
+
+    $('#people').on('change blur', function (e) {
+        var count = parseInt($(this).val(), 10),
+            required = peopleToPizzas(count);
+        $('#result').text(required + ' pizza' + pluralize(required));
+    });
+
 })(jQuery);
